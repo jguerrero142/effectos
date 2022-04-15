773649a8dae829749ac55ed1b62b51933370a97b
@julgueover.05 <julgueover.05@gmail.com>
2022-04-15 13:00:51 -0500

    alistamiento

---
+++ Angular/redux/effects/src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit {
+  usuarios: Usuario[] = []
+  constructor( public usuarioService: UsuarioService) { }
+
+  ngOnInit(): void {
+    this.usuarioService.getUser()
+    .subscribe( users =>{
+      console.log(users);
+      this.usuarios = users;
+    })
+  }
+
+}
