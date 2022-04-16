28e78788fbd1fc1eeb8074256820e254975e29fc
@julgueover.05 <julgueover.05@gmail.com>
2022-04-16 12:44:03 -0500

    stilo

--- Angular/redux/effects/src/app/store/actions/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './usuarios.actions';
-export * from './usuario.actions';
--- Angular/redux/effects/src/app/store/effects/index.ts
+++
@@ -1,6 +0,0 @@
-import { UsuariosEffects } from './usuarios.effects';
-import { UsuarioEffects } from './usuario.effects';
-
-
-
-export const EffectsArray: any[] = [UsuariosEffects, UsuarioEffects]
